2da133fa7f868bc15ed8f2806d40d56edbabf1e9
Cason Adams <casonadams@gmail.com>
2021-05-31 16:48:58 -0600

    multi scheme support

--- lua/lush_theme/nvim-nord.lua
+++ lua/lush_theme/nvim-nord.lua
@@ -525,6 +525,16 @@ local theme = lush(function()
     LspDiagnosticsUnderlineInformation   { fg = "none", gui = "none" }, -- Used to underline "Information" diagnostics
     LspDiagnosticsUnderlineHint          { fg = "none", gui = "none" }, -- Used to underline "Hint" diagnostics
 
+    LspDiagnosticsFloatingError          { fg = clrs.nord11, bg = clrs.nord0 }, -- Used to color "Error" diagnostic messages in diagnostics float
+    LspDiagnosticsFloatingWarning        { fg = clrs.nord13, bg = clrs.nord0 }, -- Used to color "Warning" diagnostic messages in diagnostics float
+    LspDiagnosticsFloatingInformation    { fg = clrs.nord8, bg = clrs.nord0 }, -- Used to color "Information" diagnostic messages in diagnostics float
+    LspDiagnosticsFloatingHint           { fg = clors.nord10, bg = clrs.nord0 }, -- Used to color "Hint" diagnostic messages in diagnostics float
+
+    LspDiagnosticsSignError              { fg = normal.red, bg = bright.black }, -- Used for "Error" signs in sign column
+    LspDiagnosticsSignWarning            { fg = bright.red, bg = bright.black }, -- Used for "Warning" signs in sign column
+    LspDiagnosticsSignInformation        { fg = normal.blue, bg = bright.black  }, -- Used for "Information" signs in sign column
+    LspDiagnosticsSignHint               { fg = normal.blue, bg = bright.black  }, -- Used for "Hint" signs in sign column
+
     LSPDiagnosticsVirtualTextWarning {
       fg = LspDiagnosticsWarning.fg.de(20).li(10),
       gui = spec.italic,
